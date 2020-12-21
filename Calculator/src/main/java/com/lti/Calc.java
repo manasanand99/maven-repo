eabf0b48c30a0b4abebdeed52577246eb7cafa6f
manas anand <manasanand99@gmail.com>
2020-12-21 14:58:33 +0530

    added sub

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,9 +5,13 @@ public class Calc {
 	public static int add(int n1,int n2) {
 		return n1+n2;
 	}
+	public static int sub(int n1,int n2) {
+		return n1-n2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(sub(30,20));
 		System.out.println(add(10,20));
 	}
 
