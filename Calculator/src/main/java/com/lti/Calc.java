10b28e3de347fdcb0cd7d50a0c3b6efa7e20e1b8
manas anand <manasanand99@gmail.com>
2020-12-21 15:16:39 +0530

    added div

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,11 +8,19 @@ public class Calc {
 	public static int sub(int n1,int n2) {
 		return n1-n2;
 	}
+	public static int mul(int n1,int n2) {
+		return n1*n2;
+	}
+	public static int div(int n1,int n2) {
+		return n1/n2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println(sub(30,20));
 		System.out.println(add(10,20));
+		System.out.println(div(100,20));
+		System.out.println(mul(10,20));
 	}
 
 }
